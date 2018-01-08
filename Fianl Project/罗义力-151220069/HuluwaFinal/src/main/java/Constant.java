58617864dc45a4083ac2e580ed3f42a0c393aa87
Yili Luo <Yili Luo>
2018-01-08 22:17:07 +0800

    Remove

--- Fianl Project/罗义力-151220069/HuluwaFinal/src/main/java/Constant.java
+++
@@ -1,6 +0,0 @@
-public interface Constant {
-    int WIDTH = 30;
-    int HEIGHT = 30;
-    String RECORD_PATH = "record/";
-
-}
--- Fianl Project/罗义力-151220069/HuluwaFinal/src/main/java/Formation.java
+++
@@ -1,5 +0,0 @@
-
-
-public interface Formation {
-    public Position nextPos();
-}
